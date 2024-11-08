d1ddbb44c6656200f1d27dbb3280d9809322b071
pmcp <github@maartenlauwaert.eu>
2024-11-08 15:38:35 +0100

    test pages 5

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,31 +1,8 @@
 <template>
-  <div class="prose mx-auto mt-12">
-
     <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
-      <article>
-      <h1 class="mb-20">{{ data.title }}</h1>
-      <ContentRenderer :value="data" class="prose" />
+      <article class="prose mx-auto mt-12">
+        <h1 class="mb-20">{{ data.title }}</h1>
+        <ContentRenderer :value="data"/>
       </article>
     </ContentQuery>
-
-<!--    <nuxt-content-->
-<!--      :document="page"-->
-<!--      class="prose w-full" />-->
-  </div>
 </template>
-
-<!--<script>-->
-<!--export default {-->
-<!--  async asyncData({ $content, params, error }) {-->
-<!--    let page;-->
-<!--    try {-->
-<!--      page = await $content("pages", params.slug).fetch();-->
-<!--    } catch (e) {-->
-<!--      error({ message: "Page not found" });-->
-<!--    }-->
-<!--    return {-->
-<!--      page-->
-<!--    };-->
-<!--  }-->
-<!--};-->
-<!--</script>-->
