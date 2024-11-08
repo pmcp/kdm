46a13df2b3908fe5817cf78ce66aa847fbdfbd33
pmcp <github@maartenlauwaert.eu>
2024-11-08 13:07:08 +0100

    added prose

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
     <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl pb-12">
       {{ page.title }}
     </h1>
-    <nuxt-link to="t-i-c">T I C</nuxt-link>
-<!--    <ContentDoc path="/pages/home" class="prose w-full" />-->
+<!--    <nuxt-link to="t-i-c">T I C</nuxt-link>-->
+    <ContentDoc path="/home" class="prose w-full" />
 
 <!--    <nuxt-content-->
 <!--      :document="page"-->
@@ -13,5 +13,5 @@
 </template>
 
 <script setup>
-const { data: page } = await useAsyncData('home', () => queryContent('pages/home').findOne())
+const { data: page } = await useAsyncData('home', () => queryContent('home').findOne())
 </script>
