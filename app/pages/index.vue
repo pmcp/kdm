adb70d1043cba0fce69e2d2c1b0fda9ecb74c644
pmcp <github@maartenlauwaert.eu>
2024-11-08 13:09:57 +0100

    removed old pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,12 +3,7 @@
     <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl pb-12">
       {{ page.title }}
     </h1>
-<!--    <nuxt-link to="t-i-c">T I C</nuxt-link>-->
     <ContentDoc path="/home" class="prose w-full" />
-
-<!--    <nuxt-content-->
-<!--      :document="page"-->
-<!--      class="prose w-full" />-->
   </div>
 </template>
 
